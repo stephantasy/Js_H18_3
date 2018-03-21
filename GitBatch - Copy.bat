6d9abf9602bafd1ba217a01be194498c50acd088
stephantasy <stephantasy@hotmail.com>
2018-03-21 14:11:28 -0400

    test 3

--- GitBatch - Copy.bat
+++ GitBatch - Copy.bat
@@ -1,10 +1,10 @@
 @echo off
 set /p var=Commit message : 
-IF NOT [%var%]==[] (
+IF NOT ["%var%"]==[] (
 	ECHO GIT ADD
 	git add .
 	ECHO GIT COMMIT
-	git commit -am %var%
+	git commit -am "%var%"
 	ECHO GIT STATUS
 	git status
 ) ELSE (
